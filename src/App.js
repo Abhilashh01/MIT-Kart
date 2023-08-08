4af9c6b50d76abc61c9bfa5575eee41e2cccca6f
Abhilashh01 <abhilashsr757@gmail.com>
2023-08-08 20:04:53 +0530

    Added routing from Landing to Home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-import Landing from "./components/landing/Landing";
+import Home from "./components/Home/Home";
+import Landing from "./components/Landing/Landing";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    Landing()
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Hero from "../../assets/Hero.svg";
 import "./Landing.css";
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const navigate = useNavigate();
   return (
     <div className="hero">
       <div className="heroCard">
         <div className="textBox">
           <h1 className="heading">Everything you need is here!</h1>
           <h6 className="heading">What are you waiting for? Let's Start Shopping...</h6>
-          <button className="cta">Get Started</button>
+          <button onClick={() => navigate("/home")} className="cta">Get Started</button>
         </div>
         <div className="imageBox">
           <img src={Hero} alt="" width={500} height={500} />
